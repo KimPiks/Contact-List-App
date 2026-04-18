7962f50586a921c73ba43dec20281912984f6247
Kamil Prorok <kamilxprorok@gmail.com>
2026-04-19 01:21:25 +0200

    build: setup docker compose

--- src/NetPC.Api/Program.cs
+++ src/NetPC.Api/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
+
+// Reverse proxy configuration
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -11,6 +20,9 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+app.UseForwardedHeaders();
+
+app.MapHealthChecks("/health");
 
 app.Run();
+
